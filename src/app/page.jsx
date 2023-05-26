679f48988a67a0fae233756f19bcb8239fc9a9ec
Nabeel Akbar <studiosnabeel@gmail.com>
2023-05-27 01:24:18 +0500

    Import and envoke the Banner component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,12 @@
 import Image from 'next/image';
 import Header from './components/Header';
+import Banner from './components/Banner';
 
 export default function Home() {
   return (
     <>
       <Header />
+      <Banner />
     </>
   );
 }
