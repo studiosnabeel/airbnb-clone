8c9765904fa8c7b4a7c5520035f848163f0d7028
Nabeel Akbar <studiosnabeel@gmail.com>
2023-05-27 12:54:01 +0500

    Add button to banner section with styling

--- src/app/components/Banner.jsx
+++ src/app/components/Banner.jsx
@@ -5,11 +5,13 @@ const Banner = () => {
   return (
     <section className="relative h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[700px]">
       <Image src={house} layout="fill" objectFit="cover" alt="BannerImg" />
-      <div
-        className="absolute top-1/2 w-full text-center
-     "
-      >
-        <p>Not sure where to go? Perfect.</p>
+      <div className="absolute top-1/2 w-full text-center">
+        <p className="text-sm md:text-lg text-white rounded-full p-2 font-bold">
+          Not sure where to go? Perfect.
+        </p>
+        <button className="text-[#329a9a] bg-white px-8 py-4 md:px-10 md:py-4 shadow-md rounded-full font-bold my-2 hover:shadow-xl active:scale-90 transition duration-150 ease-out">
+          I&apos;m flexible
+        </button>
       </div>
     </section>
   );
